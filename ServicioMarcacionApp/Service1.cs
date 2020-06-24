d5073f27d0f06902310df645a5284e555e35eb71
César Medina Romero <Cesar.Medina.romero@outlook.com>
2020-06-24 16:49:54 -0500

    primer commit funcionando servicio de marcacion

---
+++ ServicioMarcacionApp/Service1.cs
@@ -0,0 +1,104 @@
+﻿using ServicioMarcacionApp.Utilitarios;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioMarcacionApp
+{
+    public partial class Service1 : ServiceBase
+    {
+        
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        Process p;
+        Process myCSharpProcess;
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                /*Funcionando Inicio*/
+                /* p = new Process();
+                 AppConfigSettings app = new AppConfigSettings();
+
+                 string targetDir;
+
+                 targetDir = string.Format(@"" + app.GetAppSettings("rutaArchivoServicio"));   //directory where the batch script is saved.
+
+                 p.StartInfo.UseShellExecute = true;
+                 //p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                 p.StartInfo.WorkingDirectory = targetDir;
+                 p.StartInfo.FileName = app.GetAppSettings("nombreArchivoServicio");          //this is the batch script that needs to run.
+                 p.StartInfo.CreateNoWindow = false;
+                 p.Start();
+           */
+                //p.WaitForExit();
+                /*Funcionando Fin*/
+                AppConfigSettings app = new AppConfigSettings();
+                myCSharpProcess = new Process();
+                myCSharpProcess.StartInfo.FileName = "java.exe";
+                myCSharpProcess.StartInfo.Arguments = "-jar "+ app.GetAppSettings("rutaArchivoServicio")+"\\"+ app.GetAppSettings("nombreArchivoServicio")+"";
+                myCSharpProcess.StartInfo.CreateNoWindow = false;
+                //myProcess.StartInfo.CreateNoWindow = true;
+                myCSharpProcess.Start();
+                EscribeVisorEventos("Se inició con éxito el Servicio MarcacionApp", null, 1);
+
+
+            }
+            catch (Exception ex)
+            {
+                EscribeVisorEventos("Error al iniciar el Servicio MarcacionApp", ex, 2);
+                OnStop();
+            }
+
+
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                myCSharpProcess.Kill();
+                EscribeVisorEventos("Se cierra con éxito el Servicio MarcacionApp", null, 1);
+            }
+            catch (Exception ex)
+            {
+                EscribeVisorEventos("Error al cerrar el Servicio MarcacionApp", ex, 2);
+            }
+        }
+
+        private void EscribeVisorEventos(string procedencia, Exception ex, int tipo)
+        {
+            try
+            {
+                if (!System.Diagnostics.EventLog.SourceExists("Servicio MarcacionApp"))
+                    System.Diagnostics.EventLog.CreateEventSource("Servicio MarcacionApp", "Application");
+                else
+                {
+                    if (tipo == 1)
+                    {
+                        System.Diagnostics.EventLog.WriteEntry("Servicio MarcacionApp", "" + procedencia + ". ",
+                             System.Diagnostics.EventLogEntryType.Information, 20, 10);
+                    }
+                    else
+                    {
+                        System.Diagnostics.EventLog.WriteEntry("Servicio MarcacionApp", "" + procedencia + ". " + ex.Message,
+                            System.Diagnostics.EventLogEntryType.Error, 20, 10);
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                
+            }
+        }
+    }
+}
---
+++ ServicioMarcacionApp/Utilitarios/AppConfigSettings.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioMarcacionApp.Utilitarios
+{
+    public class AppConfigSettings
+    {
+        Configuration config;
+
+        public AppConfigSettings()
+        {
+            config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        }
+
+        public string GetConnectionString(string key)
+        {
+            return config.ConnectionStrings.ConnectionStrings[key].ConnectionString;
+        }
+
+        public void SaveConnectionString(string key, string value)
+        {
+            ConnectionStringsSection section = config.GetSection("connectionStrings") as ConnectionStringsSection;
+            //if (section.SectionInformation.IsProtected)
+            //{
+            //    section.ConnectionStrings[key].ConnectionString = "metadata=res://*/Entidades.DatosModelo.csdl|res://*/Entidades.DatosModelo.ssdl|res://*/Entidades.DatosModelo.msl;provider=System.Data.SqlClient;provider connection string=\"" + value + "\"";
+            //}
+            //else
+            //{
+            section.ConnectionStrings[key].ConnectionString = "metadata=res://*/Entidades.TramaDB.csdl|res://*/Entidades.TramaDB.ssdl|res://*/Entidades.TramaDB.msl;provider=System.Data.SqlClient;provider connection string=\"" + value + "\"";
+            //    section.SectionInformation.ProtectSection("DataProtectionConfigurationProvider");
+            //}
+            config.Save();
+        }
+
+        public void SaveAppSettings(string key, string value)
+        {
+            AppSettingsSection section = config.GetSection("appSettings") as AppSettingsSection;
+
+            //if (section.SectionInformation.IsProtected)
+            //{
+            //    config.AppSettings.Settings[key].Value = value;
+            //}
+            //else
+            //{
+            config.AppSettings.Settings[key].Value = value;
+            //    section.SectionInformation.ProtectSection("DataProtectionConfigurationProvider");
+            //}
+            config.Save();
+        }
+
+        public string GetAppSettings(string key)
+        {
+            string value = "";
+            try
+            {
+                value = config.AppSettings.Settings[key].Value;
+            }
+            catch (Exception)
+            {
+
+            }
+            return value;
+        }
+    }
+}
